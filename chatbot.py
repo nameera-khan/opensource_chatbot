b8c1b9d7c4d1b96961923759444bd4ad9fc5ff0d
Nameera Nilofer Khan <82845744+nameera-khan@users.noreply.github.com>
2025-11-25 08:09:15 +0400

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,15 @@
 import streamlit as st
 
-set.set_page_config(
+st.set_page_config(
     page_title = 'ask for data',
     page_icon = '🥲',
     layout = 'wide'
 )
 st.title('ask for data')
+st.markdown('upload data and ask away')
+
+
+with st.sidebar:
+    st.header('data upload')
+    uploaded_file = st.file_uploader('upload csv',type='csv')
+
